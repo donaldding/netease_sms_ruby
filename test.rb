09e8dad9b6adba1a75154c4af3802d29b7447f9a
donaldding <donaldding@163.com>
2018-03-23 21:43:33 +0800

    remove test.rb

--- test.rb
+++
@@ -1,7 +0,0 @@
-require 'netease_sms'
-include NeteaseSms
-
-c = Client.new('appkey', 'appsecret')
-cellphones = ["18575750000","18316960000"]
-params=["1234"]
-c.send_sms("1234", cellphones, params)
